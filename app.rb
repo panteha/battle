8f7fd2f3da2eb6b44b455d76e60b1fb5574606be
Aaron Rodrigues <aaronrodrigues@Aarons-MacBook-Pro.local>
2017-05-24 15:11:48 +0100

    Added feature test for entering names

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
 require 'sinatra/base'
 
-
-
-
 class Battle < Sinatra::Base
 
   get '/' do
-    "Testing infrastructure working!"
+    "Battle!!"
+    erb :index
+  end
+
+  get '/names' do
+    @name = params[:name]
+    erb :index
+  end
+
+  post '/names' do
+    @name = params[:name]
+    erb :play
   end
 
    run! if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,9 +1,15 @@
-# require_relative 'app'
-# require 'capybara/rspec'
+# feature 'checks the home page returning a message' do
+#   scenario 'returns a string' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
 
-  feature 'checks the home page returning a message' do
-    scenario 'returns a string' do
-      visit('/')
-      expect(page).to have_content "Testing infrastructure working!"
+feature 'Players can fill in their names' do
+  scenario 'fill in name' do
+   visit('/')
+   fill_in('name', with: 'John')
+   click_button('Submit')
+   expect(page).to have_content('John')
   end
 end
