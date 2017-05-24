2d422bb9a6e856cc1b1de72df4854e0ab5802ad2
Aaron Rodrigues <aaronrodrigues@Aarons-MacBook-Pro.local>
2017-05-24 16:17:48 +0100

    Added player 2 hit points

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -14,3 +14,14 @@ feature 'Enter names' do
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
+
+feature 'See player 2s Hit Points' do
+  scenario ' Hit points ' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Mittens: 60HP'
+
+  end
+end
