71a7d1fb994ba7785c9aa6c064a0f5c54be951f0
Panteha <panteha.ahmadi@gmail.com>
2017-05-25 22:29:16 +0100

    Added reduce_pints method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,20 @@
 class Player
 
-attr_reader :name
+attr_reader :name, :hit_points
 
-  def initialize(name)
+DEFAULT_HITPOINTS = 60
+
+  def initialize(name, hit_points = DEFAULT_HITPOINTS )
     @name = name
+    @hit_points = hit_points
+  end
+
+  def attack(player)
+    player.reduce_points
+  end
+
+  def reduce_points
+    @hit_points -= 10
   end
 
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -10,7 +10,7 @@ end
 feature 'See Hit Points' do
   scenario 'See player 2 Hit points ' do
     sign_in_and_play
-    expect(page).to have_content 'Paul: 60HP'
+    expect(page).to have_content 'Paul: 60 HP'
   end
 end
 
@@ -18,6 +18,14 @@ end
     scenario 'Attack player 2 and wants to get a confirmation' do
        sign_in_and_play
        click_link 'Attack'
-       expect(page).to have_content "Peter attacked Paul"
+       expect(page).to have_content 'Peter attacked Paul'
+    end
+  end
+
+  feature 'Reduce points' do
+    scenario 'Player 1\'s attack reduces the hit point of player 2\'s' do
+      sign_in_and_play
+      click_link 'Attack'
+      expect(page).to have_content 'Paul: 50 HP'
     end
   end
